50d5c00da165e50e5e8a3263d3b70342c0920f5a
Ante Kresic <ante.kresic@gmail.com>
2020-05-11 11:41:45 +0200

    Add static deployment and generation script
    Users which want to use static deployment resource to deploy to a
    Kubernetes cluster can use the generated script instead of needing to
    depend on Helm services.

---
+++ generate-deploy-script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ -n "$DEBUG" ]; then
+	set -x
+fi
+
+# Default values.yaml file to provide to helm template command.
+FILE_ARG=./chart/values.yaml
+
+# Check if values file filepath was supplied.
+if [ "$#" -eq  "1" ]; then
+	if [ -f "$1" ]; then
+		FILE_ARG=$1
+	fi
+fi
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+DIR=$(cd $(dirname "${BASH_SOURCE}") && pwd -P)
+
+RELEASE_NAME=ts-observability
+NAMESPACE=ts-observability
+
+NAMESPACE_VAR="
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: $NAMESPACE
+  labels:
+    app.kubernetes.io/name: $RELEASE_NAME
+    app.kubernetes.io/instance: timescale-observability
+"
+
+OUTPUT_FILE="${DIR}/deploy/static/deploy.yaml"
+cat << EOF | helm template $RELEASE_NAME ${DIR}/chart --namespace $NAMESPACE --values $FILE_ARG > ${OUTPUT_FILE}
+EOF
+
+echo "${NAMESPACE_VAR}
+$(cat ${OUTPUT_FILE})" > ${OUTPUT_FILE}
