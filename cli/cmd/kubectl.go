4b6118dfc5381a447aacd7304f9443af17d2eb09
tuapuikia <dknyvice@gmail.com>
2020-10-08 15:56:24 +0800

    Add auth support for kubectl cmd
    Signed-off-by: tuapuikia <dknyvice@gmail.com>

--- cli/cmd/kubectl.go
+++ cli/cmd/kubectl.go
@@ -14,6 +14,7 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	rest "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/portforward"
