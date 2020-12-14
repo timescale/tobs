76ce4daf6f30e9b4576849bebe4771138fe721bb
Vineeth Pothulapati <vineethpothulapati@outlook.com>
2020-12-14 19:39:52 +0530

    Add flag to restart pods on pvc expansion
    Signed-off-by: Vineeth Pothulapati <vineethpothulapati@outlook.com>

--- cli/tests/volume_test.go
+++ cli/tests/volume_test.go
@@ -3,12 +3,14 @@ package tests
 import (
 	"errors"
 	"github.com/timescale/tobs/cli/pkg/k8s"
+	v1 "k8s.io/api/core/v1"
 	"os/exec"
 	"strings"
 	"testing"
+	"time"
 )
 
-func testVolumeExpansion(t testing.TB, timescaleDBStorage, timescaleDBWal, prometheusStorage string) {
+func testVolumeExpansion(t testing.TB, timescaleDBStorage, timescaleDBWal, prometheusStorage string, restartPods bool) {
 	cmds := []string{"volume", "expand", "-n", RELEASE_NAME, "--namespace", NAMESPACE}
 	if timescaleDBStorage != "" {
 		cmds = append(cmds, "--timescaleDB-storage", timescaleDBStorage)
@@ -22,11 +24,14 @@ func testVolumeExpansion(t testing.TB, timescaleDBStorage, timescaleDBWal, prome
 		cmds = append(cmds, "--prometheus-storage", prometheusStorage)
 	}
 
+	if restartPods {
+		cmds = append(cmds, "--restart-pods")
+	}
+
 	t.Logf("Running '%v'", "tobs "+strings.Join(cmds, " "))
 	 expand := exec.Command("./../bin/tobs", cmds...)
-	 out, err := expand.CombinedOutput()
+	 _, err := expand.CombinedOutput()
 	 if err != nil {
-		 t.Logf(string(out))
 		 t.Fatal(err)
 	 }
 }
@@ -49,7 +54,6 @@ func testVolumeGet(t testing.TB, timescaleDBStorage, timescaleDBWal, prometheusS
 	expand := exec.Command("./../bin/tobs", cmds...)
 	out, err := expand.CombinedOutput()
 	if err != nil {
-		t.Logf(string(out))
 		t.Fatal(err)
 	}
 
@@ -98,7 +102,7 @@ func TestVolume(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	testVolumeExpansion(t, "151Gi", "21Gi", "9Gi")
+	testVolumeExpansion(t, "151Gi", "21Gi", "9Gi", false)
 	res, err := k8s.GetAllPVCSizes()
 	if err != nil {
 		t.Fatal(err)
@@ -107,7 +111,7 @@ func TestVolume(t *testing.T) {
 		t.Fatal(errors.New("failed to verify volume expansion test-1"))
 	}
 
-	testVolumeExpansion(t, "152Gi", "22Gi", "")
+	testVolumeExpansion(t, "152Gi", "22Gi", "", false)
 	res, err = k8s.GetAllPVCSizes()
 	if err != nil {
 		t.Fatal(err)
@@ -116,7 +120,7 @@ func TestVolume(t *testing.T) {
 		t.Fatal(errors.New("failed to verify volume expansion test-2"))
 	}
 
-	testVolumeExpansion(t, "153Gi", "", "")
+	testVolumeExpansion(t, "153Gi", "", "", false)
 	res, err = k8s.GetAllPVCSizes()
 	if err != nil {
 		t.Fatal(err)
@@ -125,7 +129,7 @@ func TestVolume(t *testing.T) {
 		t.Fatal(errors.New("failed to verify volume expansion test-3"))
 	}
 
-	testVolumeExpansion(t, "", "23Gi", "")
+	testVolumeExpansion(t, "", "23Gi", "", false)
 	res, err = k8s.GetAllPVCSizes()
 	if err != nil {
 		t.Fatal(err)
@@ -134,7 +138,7 @@ func TestVolume(t *testing.T) {
 		t.Fatal(errors.New("failed to verify volume expansion test-4"))
 	}
 
-	testVolumeExpansion(t, "", "24Gi", "10Gi")
+	testVolumeExpansion(t, "", "24Gi", "10Gi", false)
 	res, err = k8s.GetAllPVCSizes()
 	if err != nil {
 		t.Fatal(err)
@@ -143,7 +147,7 @@ func TestVolume(t *testing.T) {
 		t.Fatal(errors.New("failed to verify volume expansion test-5"))
 	}
 
-	testVolumeExpansion(t, "", "", "11Gi")
+	testVolumeExpansion(t, "", "", "11Gi", false)
 	res, err = k8s.GetAllPVCSizes()
 	if err != nil {
 		t.Fatal(err)
@@ -151,4 +155,164 @@ func TestVolume(t *testing.T) {
 	if res["storage-volume-gg-timescaledb-0"] != "153Gi" &&  res["wal-volume-gg-timescaledb-0"] != "24Gi" && res["gg-prometheus-server"] != "11Gi"{
 		t.Fatal(errors.New("failed to verify volume expansion test-6"))
 	}
+
+	timescaleDBLabels := map[string]string{"app": "gg-timescaledb", "release": "gg"}
+	prometheusLabels := map[string]string{"app": "prometheus", "component": "server", "release": "gg"}
+
+	// TESTCASE: Volume expand Prometheus storage and restart the pods
+
+	pods, err := k8s.KubeGetPods("ns",  prometheusLabels)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var podsSet []podDetails
+	for _, p := range pods {
+		if p.Status.Phase == "Running" {
+			details := podDetails{
+				name:                  p.Name,
+				oldPodCreateTimestamp: p.CreationTimestamp.String(),
+			}
+			podsSet = append(podsSet, details)
+		}
+	}
+
+	testVolumeExpansion(t, "", "", "12Gi", true)
+	res, err = k8s.GetAllPVCSizes()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res["storage-volume-gg-timescaledb-0"] != "153Gi" &&  res["wal-volume-gg-timescaledb-0"] != "24Gi" && res["gg-prometheus-server"] != "12Gi"{
+		t.Fatal(errors.New("failed to verify volume expansion test-6"))
+	}
+
+	time.Sleep(8 * time.Second)
+	var labels []map[string]string
+	labels = append(labels, timescaleDBLabels)
+	verifyPodRestart(t, podsSet, labels)
+
+	// TESTCASE: Volume expand TimescaleDB storage, wal, Prometheus storage & restart the pods
+
+	// sleep between test executions
+	time.Sleep(30 * time.Second)
+	pods, err = k8s.KubeGetPods("ns",  prometheusLabels)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	podsSet = []podDetails{}
+	labels = []map[string]string{}
+	for _, p := range pods {
+		if p.Status.Phase == "Running" {
+			details := podDetails{
+				name:                  p.Name,
+				oldPodCreateTimestamp: p.CreationTimestamp.String(),
+			}
+			podsSet = append(podsSet, details)
+		}
+	}
+
+	pods, err = k8s.KubeGetPods("ns", timescaleDBLabels)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, p := range pods {
+		if p.Status.Phase == "Running" {
+			details := podDetails{
+				name:                  p.Name,
+				oldPodCreateTimestamp: p.CreationTimestamp.String(),
+			}
+			podsSet = append(podsSet, details)
+		}
+	}
+
+	testVolumeExpansion(t, "154Gi", "24Gi", "13Gi", true)
+	res, err = k8s.GetAllPVCSizes()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res["storage-volume-gg-timescaledb-0"] != "154Gi" &&  res["wal-volume-gg-timescaledb-0"] != "24Gi" && res["gg-prometheus-server"] != "13Gi"{
+		t.Fatal(errors.New("failed to verify volume expansion test-6"))
+	}
+
+	// As timescaleDB pod is an statefulset bounded to a PVC
+	// it takes some time to move to terminating state
+	time.Sleep(1 * time.Minute)
+
+	labels = append(labels, timescaleDBLabels, prometheusLabels)
+	verifyPodRestart(t, podsSet, labels)
+
+	// TESTCASE: Volume Expand only timescaleDB and restart pods.
+
+
+	// sleep between test executions
+	time.Sleep(30 * time.Second)
+	pods, err = k8s.KubeGetPods("ns", map[string]string{"app": "gg-timescaledb", "release": "gg"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	podsSet = []podDetails{}
+	labels = []map[string]string{}
+	for _, p := range pods {
+		if p.Status.Phase == "Running" {
+			details := podDetails{
+				name:                  p.Name,
+				oldPodCreateTimestamp: p.CreationTimestamp.String(),
+			}
+			podsSet = append(podsSet, details)
+		}
+	}
+
+	testVolumeExpansion(t, "155Gi", "25Gi", "", true)
+	res, err = k8s.GetAllPVCSizes()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res["storage-volume-gg-timescaledb-0"] != "155Gi" &&  res["wal-volume-gg-timescaledb-0"] != "25Gi" && res["gg-prometheus-server"] != "13Gi"{
+		t.Fatal(errors.New("failed to verify volume expansion test-6"))
+	}
+
+	// As timescaleDB pod is an statefulset bounded to a PVC
+	// it takes some time to move to terminating state
+	time.Sleep(1 * time.Minute)
+
+	labels = append(labels, timescaleDBLabels)
+	verifyPodRestart(t, podsSet, labels)
+
+}
+
+func verifyPodRestart(t *testing.T, podsSet []podDetails, labels []map[string]string) {
+	var pods []v1.Pod
+	for _, l := range labels {
+		p, err := k8s.KubeGetPods("ns", l)
+		if err != nil {
+			t.Fatal(err)
+		}
+		pods = append(pods, p...)
+	}
+
+	podsCounter := 0
+	for _, p := range pods {
+		if p.Status.Phase == "Running" {
+			podsCounter++
+			for _, v := range podsSet {
+				// compare names & timestamp as statefulsets will have same name then compare create timestamp
+				if v.name == p.Name && v.oldPodCreateTimestamp == p.CreationTimestamp.String() {
+					t.Fatal(errors.New("failed to restart the pod after volume expansion " + v.name))
+				}
+			}
+		}
+	}
+
+	// validate number of pods killed are equal to number of pods running i.e expected to be restarted.
+	if len(podsSet) != podsCounter {
+			t.Fatal(errors.New("failed to restart pods after volume expansion"))
+	}
+}
+
+type podDetails struct {
+	name string
+	oldPodCreateTimestamp string
+	newPodCreateTimestamp string
 }
