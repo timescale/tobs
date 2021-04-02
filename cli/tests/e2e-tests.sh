b3df7136848fa69abf1abd6ccc3f8c3ec156b7a1
Vineeth Pothulapati <vineethpothulapati@outlook.com>
2021-04-02 14:53:12 +0530

    test ci docker, kind version with latest

--- cli/tests/e2e-tests.sh
+++ cli/tests/e2e-tests.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-kind create cluster --name tobs
+#kind create cluster --name tobs
+#
+#clean_up() {
+#  ec=$?
+#  kind delete cluster --name tobs
+#  exit $ec
+#}
 
-clean_up() {
-  ec=$?
-  kind delete cluster --name tobs
-  exit $ec
-}
-
-trap clean_up SIGHUP SIGINT SIGTERM
-go test -v ./tests/tobs-cli-tests --timeout 30m
-clean_up
+#trap clean_up SIGHUP SIGINT SIGTERM
+#go test -v ./tests/tobs-cli-tests --timeout 30m
+#clean_up
