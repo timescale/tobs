ab3ee3d65fd20ef47d2e890b8430fbc773c95444
Matvey Arye <mat@timescale.com>
2021-10-08 08:28:21 -0700

    Release 0.7.0

--- cli/cmd/version/version.go
+++ cli/cmd/version/version.go
@@ -9,7 +9,7 @@ import (
 	"github.com/timescale/tobs/cli/pkg/utils"
 )
 
-const tobsVersion = "0.6.1"
+const tobsVersion = "0.7.0"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
